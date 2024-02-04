a1a39d41c8264ed326a7f0c94eb1e126f493b624
MaksimDubovyi <maksim.dubovyi.ua@gmail.com>
2024-02-05 01:44:26 +0300

    test5

--- src/app/components/main/Main.jsx
+++ src/app/components/main/Main.jsx
@@ -12,12 +12,31 @@ const Main = () => {
         <div className={style.mainContent}>
           {arr.map((item) => (
             <div key={item} className={style.mainContentItem}>
-              <div className={style.itemImage}></div>
+              {/* <div className={style.itemImage}></div> */}
+              <Image
+                src={"/icon-main/imgmain.webp"}
+                alt={item.alt}
+                priority={true}
+                width={347}
+                height={171}
+                loading="eager"
+              />
               <h3 className={style.itemImage}>Lorem Ipsum is simply dummy</h3>
             </div>
           ))}
         </div>
-        <div className={style.mainBlockImg}></div>
+        {/* <div className={style.mainBlockImg}></div> */}
+        <div>
+          <Image
+            className={style.mainBlockImg}
+            src={"/icon-main/ads.webp"}
+            alt={"item.alt"}
+            priority={true}
+            width={300}
+            height={600}
+            loading="eager"
+          />
+        </div>
       </div>
     </div>
   );
